1526af544d8c0db93315765c80130c0c79a8b609
quentintrouve <trouve.quentin@gmail.com>
2020-12-10 22:10:48 +0100

    add axios.post for new article

--- src/composants/NvPost/NvPost.js
+++ src/composants/NvPost/NvPost.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, { Component } from 'react'
 
 import './NvPost.css'
@@ -10,21 +11,39 @@ class NvPost extends Component {
         author: 'Hugo'
     }
 
-    render () {
+    addpost = () => {
+        const newPost = {
+            title: this.state.title,
+            body: this.state.content,
+            author: this.state.author
+        }
+        axios.post('https://jsonplaceholder.typicode.com/posts', newPost)
+            .then(response => { console.log(response) })
+
+        this.setState = ({
+            title: '',
+            content: '',
+            author: 'Hugo'
+        })
+    }
+
+
+
+    render() {
         return (
             <div className="NvPost form-group">
                 <h1>Ajouter un Article</h1>
                 <label>Titre</label>
-                <input className="form-control" type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
+                <input className="form-control" type="text" value={this.state.title} onChange={(event) => this.setState({ title: event.target.value })} />
                 <label>Contenu</label>
-                <textarea className="form-control" rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+                <textarea className="form-control" rows="4" value={this.state.content} onChange={(event) => this.setState({ content: event.target.value })} />
                 <label>Auteur</label>
-                <select className="form-control" value= {this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
+                <select className="form-control" value={this.state.author} onChange={(event) => this.setState({ author: event.target.value })}>
                     <option value="Hugo">Hugo</option>
                     <option value="Juliette">Juliette</option>
                     <option value="John">John</option>
                 </select>
-                <button className="btn btn-success my-3">Ajouter un Article</button>
+                <button className="btn btn-success my-3" onClick={this.addpost}>Ajouter un Article</button>
             </div>
         );
     }
